99ad7017b516c2253a5d7a1281b094e9e6b3ac84
Artem Kamyshan <artem.kamyshan@yandex.ru>
2023-02-12 17:22:01 +0300

    added goods list

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,21 @@
+function Preloader() {
+    return (
+        <div className="preloader center-align">
+            <div className="preloader-wrapper active">
+                <div className="spinner-layer spinner-green-only">
+                    <div className="circle-clipper left">
+                        <div className="circle"></div>
+                    </div>
+                    <div className="gap-patch">
+                        <div className="circle"></div>
+                    </div>
+                    <div className="circle-clipper right">
+                        <div className="circle"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export { Preloader };
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,32 @@
 import { useState, useEffect } from "react";
+import { API_KEY, API_URL } from "../config";
+
+import { Preloader } from "./Preloader";
+import { GoodsList } from "./GoodsList";
 
 function Shop() {
-    return <main className="container content">Shop</main>;
+    const [goods, setGoods] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(function getGoods() {
+        fetch(API_URL, {
+            headers: {
+                Authorization: API_KEY,
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                data.featured && setGoods(data.featured);
+                setLoading(false);
+            });
+    }, []);
+
+    console.log(API_KEY);
+    return (
+        <main className="container content">
+            {loading ? <Preloader /> : <GoodsList goods={goods} />}
+        </main>
+    );
 }
 
 export { Shop };
